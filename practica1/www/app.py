25cdc43de7b5e70a4b23ef57cd024b54040a78bb
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2018-08-23 17:44:46 -0300

    Index - Result - App

--- practica1/www/app.py
+++ practica1/www/app.py
@@ -1,6 +1,7 @@
 # Imports
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,6 +9,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
 
 if __name__ == "__main__":
     app.run(host='192.168.99.100', port=8888)
