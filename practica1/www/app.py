bad854050586b66a97c1a9ff9e589dd89a5b2243
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2018-08-24 17:10:07 -0300

    actualizacion

--- practica1/www/app.py
+++ practica1/www/app.py
@@ -28,42 +28,45 @@ def contact():
 def about():
     return render_template("about.html")
 
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/result.html',methods = ['POST', 'GET'])
 def result():
+	
+	pro.start_process()
+	average=Samples()
+	last_meas=Samples()
+	
+	'''
+	db=Database()
+	last_meas = db.get_last_sample()
+	average = db.get_average_sample()
+	'''
+
+	period = 0
+	average.temperature = 56.4
+	average.humidity = 951.7
+	average.pressure = 43.1
+	average.windspeed = 298.9
+	last_meas.temperature = 21
+	last_meas.humidity = 38
+	last_meas.pressure = 901
+	last_meas.windspeed = 245
+
+	test = [
+		{ "temp" : average.temperature,"hum" : average.humidity,"pre" : average.pressure, "wind": average.windspeed},
+		{ "temp" : last_meas.temperature,"hum" : last_meas.humidity,"pre" : last_meas.pressure, "wind": last_meas.windspeed}
+	]
+	
+	test = json.dumps(test)
+
+
 	if request.method == 'POST':
-		#a = request.form['period']
-		pro.start_process()
-		average=Samples()
-		last_meas=Samples()
-
-		'''
-		average.temperature = 56.4
-		average.humidity = 951.7
-		average.pressure = 43.1
-		average.windspeed = 298.9
-		last_meas.temperature = 21
-		last_meas.humidity = 38
-		last_meas.pressure = 901
-		last_meas.windspeed = 245
-		'''
-
-
-		db=Database()
-		last_meas = db.get_last_sample()
-		average = db.get_average_sample()
-
-		test = [
-			{ "temp" : average.temperature,"hum" : average.humidity,"pre" : average.pressure, "wind": average.windspeed},
-			{ "temp" : last_meas.temperature,"hum" : last_meas.humidity,"pre" : last_meas.pressure, "wind": last_meas.windspeed}
-		]
-		#test = [name=1,2,3,4,5,6]
-		test = json.dumps(test)
-		return render_template('result.html', test=test)
+		period = request.form['period']
 	else:
-		return render_template('result.html')
+		period = request.args.get('period')
 
+	return render_template('result.html', test=test,reload=period)
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=8888)
+    app.run(host='198.', port=8888)
     #app.run(host='http://localhost', port=8888)
 
