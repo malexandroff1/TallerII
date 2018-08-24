991cdb184188f7efdceed304eda2f8b01978d892
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2018-08-23 21:49:16 -0300

    actualizaciones

--- practica1/www/app.py
+++ practica1/www/app.py
@@ -1,7 +1,7 @@
 # Imports
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import redirect, url_for, request, render_template, Flask
+from flask import jsonify
+from flask import json
 
 app = Flask(__name__)
 
@@ -9,11 +9,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/index2')
+def index2():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
+	if request.method == 'POST':
+		a = request.form['period']
+		jsonStr = json.dumps(a)
+		return jsonify(result=jsonStr)
+	else:
+		return render_template("result.html")
+
 
 if __name__ == "__main__":
     app.run(host='192.168.99.100', port=8888)
