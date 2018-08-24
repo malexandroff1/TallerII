661c812364e517378688c186c3e62492d6dc669e
malexandroff1 <m.alexandroff1@gmail.com>
2018-08-24 09:50:09 -0300

    se crean funciones de adquisicion de datos de la db

--- practica1/www/database.py
+++ practica1/www/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import desc
+from models import Samples
 import os
 
 class Database(object):
@@ -13,7 +14,7 @@ class Database(object):
     db_port = os.getenv("DB_PORT") if os.getenv("DB_PORT") != None else "3306"
     Base = declarative_base()
     
-    def get_session(self):
+    def _get_session(self):
         """Singleton of db connection
 
         Returns:
@@ -27,4 +28,34 @@ class Database(object):
             self.session = Session()
             self.Base.metadata.create_all(engine)
         return self.session
-    
+    def get_last_sample(self):
+        session=self._get_session()
+        query = session.query(Samples).order_by(desc(Samples.id))
+        sample = query.first()
+        session.close()
+        return sample
+    def insert_sample(self, sample):
+        session=self._get_session()
+        session.add(sample)
+        session.commit()
+        session.close()
+    def get_average_sample(self):
+        temp=0
+        humedad=0
+        presion=0
+        viento=0
+        session=self._get_session()
+        query = session.query(Samples).order_by(desc(Samples.id)).limit(10)
+        samples = query.all()
+        for s in samples:
+            temp=temp+s.temperature
+            humedad=humedad+s.humidity
+            presion=presion+s.pressure
+            viento=viento+s.windspeed
+        average=Samples()
+        average.temperature=float(temp)/10
+        average.humidity=float(humedad)/10
+        average.pressure=float(presion)/10
+        average.windspeed=float(viento)/10
+        session.close()
+        return average
--- practica1/www/process.py
+++ practica1/www/process.py
@@ -4,14 +4,11 @@ import time
 from random import randint
 
 db=Database()
-session=db.get_session()
 while True:
 	sample=Samples()
 	sample.temperature=	randint(0, 100)
 	sample.humidity=randint(0, 100)
 	sample.pressure=randint(900, 1024)
 	sample.windspeed=randint(25, 500)
-	session.add(sample)
-	session.commit()
+	db.insert_sample(sample)
 	time.sleep(1)
-session.close()
