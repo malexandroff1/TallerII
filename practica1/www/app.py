c51a527a1bf4134990080bb79b289124371b953d
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2018-08-26 17:40:18 -0300

    Actualización

--- practica1/www/app.py
+++ practica1/www/app.py
@@ -17,18 +17,6 @@ def index():
 def index2():
     return render_template('index.html')
 
-@app.route('/services')
-def services():
-    return render_template('services.html')
-
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
 @app.route('/metrics.html',methods = ['POST', 'GET'])
 def metrics():
 	
