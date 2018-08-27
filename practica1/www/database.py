e921f54cd7316f006d0a8d5457a0d8c637dc36e9
malexandroff1 <m.alexandroff1@gmail.com>
2018-08-27 11:27:22 -0300

    se corrigen bugs

--- practica1/www/database.py
+++ practica1/www/database.py
@@ -28,34 +28,49 @@ class Database(object):
             self.session = Session()
             self.Base.metadata.create_all(engine)
         return self.session
+    #funcion que retorna la ultima medida almacenada en la base de datos
+    #retorna un objeto sample
     def get_last_sample(self):
+        #obtiene la sesion
         session=self._get_session()
+        #obtiene las medidas ordenadas por id en forma descendente
         query = session.query(Samples).order_by(desc(Samples.id))
+        #obtiene la ultima medida
         sample = query.first()
         session.close()
         return sample
+    #funcion que inserta las mediciones a la base de datos
+    #parametro de entrada:
+    #objeto sample
     def insert_sample(self, sample):
         session=self._get_session()
         session.add(sample)
         session.commit()
         session.close()
+    #funcion que retorna el promedio de las ultimas 10 mediciones
+    #retorna un objeto sample con el promedio de cada medida en sus atributos
     def get_average_sample(self):
         temp=0
         humedad=0
         presion=0
         viento=0
+        #obtiene la sesion
         session=self._get_session()
+        #obtiene 10 medidas ordenadas por id en forma descendente
         query = session.query(Samples).order_by(desc(Samples.id)).limit(10)
         samples = query.all()
+        #suma los valores de las ultimas 10 medidas
         for s in samples:
             temp=temp+s.temperature
             humedad=humedad+s.humidity
             presion=presion+s.pressure
             viento=viento+s.windspeed
         average=Samples()
+        #calcula el promedio
         average.temperature=float(temp)/10
         average.humidity=float(humedad)/10
         average.pressure=float(presion)/10
         average.windspeed=float(viento)/10
         session.close()
+        #retorna el objeto
         return average
