564a6102d8101febcc5e4e7a35332f22789de59f
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2018-08-30 21:53:58 -0300

    validación de formulario y actualización de links de ayuda

--- practica1/www/app.py
+++ practica1/www/app.py
@@ -6,6 +6,27 @@ from flask import json
 from database import Database
 from models import Samples
 from getMetrics import _get_metrics 
+ 
+numbers = [0 , 1000, 2000, 5000, 10000, 30000, 60000]
+
+
+def validate_movie_data(form):
+    form.errors = {}
+
+    if len(form['period'].strip()) == 0:
+        form.errors['period'] = 'Period can not be blank.'
+    elif not form['period'].isdigit():
+    	form.errors['period'] = 'Period can not be string.' 
+    else:
+    	ok = False
+    	array_length = len(numbers)
+    	for i in range(array_length):
+    		if numbers[i] == int(form['period']):
+    			ok = True
+    	if not ok :
+    		form.errors['period'] = 'Period can be between correct values.'
+
+    return len(form.errors) == 0
 
 
 app = Flask(__name__)
@@ -15,13 +36,15 @@ pro = Process()
 #Cuando se ejecuta /index se redirige a la pagina index.html
 @app.route('/')
 def index():
-	return render_template('index.html')
+	form = {'period': ''}
+	return render_template('index.html', form=form)
 
 ###*
 #Cuando se ejecuta /index2 se regirige a la pagina index.html
 @app.route('/index2')
 def index2():
-	return render_template('index.html')
+	form = {'period': ''}
+	return render_template('index.html', form=form)
 
 ###*
 #Funcion que ejecuta el metodo _get_metrics()
@@ -51,18 +74,19 @@ def metrics():
 	#Si el método del formulario es POST o GET se obtiene el periodo.
 	#Si se envian parametros por la URL tambien se obtiene los datos
 	if request.method == 'POST':
-		period = request.form['period']
-	else:
-		if request.method == 'GET':
-			period = request.args.get('period')
-
-	###*
-	#Redirigimos al sitio metrics.html y le enviamos las variables values y period
+		valid = validate_movie_data(request.form)
+		if valid:
+			period = request.form['period']
+			return render_template('metrics.html', test=values, reload=period)
+		form = request.form
+		return render_template('index.html', form=form)
 	return render_template('metrics.html', test=values, reload=period)
 
 ###*
 #Si se ejecuta este archivo el main se toma en cuenta, sino no.
 if __name__ == "__main__":
-	#app.run(host='http://192.168.99.100', port=8888)
-	app.run(host='http://localhost', port=8888)
+	app.run(host='http://192.168.99.100', port=8888)
+	#app.run(host='http://localhost', port=8888)
 
+###*
+#Redirigimos al sitio metrics.html y le enviamos las variables values y period
