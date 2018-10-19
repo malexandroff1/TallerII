aa37d71eeeb9b387b783d858a4f01b6167b4668c
root <root@orangepi.(none)>
2018-10-19 19:27:25 +0000

    actualización de carpetas

--- orange/testFlask/test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def test():
-    return "Esto es una prueba de servidor Flask"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8800, debug=True)
